dff3f3bcebfb7fa6595be6cad5fcb8833889ff8f
GunnarPeipman <gunnar@dt.ee>
2010-10-30 03:52:54 +0300

    added numeric textbox example

--- WindowsPhone7Examples/UiElements/App.xaml.cs
+++ WindowsPhone7Examples/UiElements/App.xaml.cs
@@ -1,21 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
+﻿using System.Windows;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 
 namespace UiElements
 {
-    public partial class App : Application
+    public partial class App
     {
         /// <summary>
         /// Provides easy access to the root frame of the Phone Application.
@@ -35,7 +25,7 @@ namespace UiElements
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 // Display the current frame rate counters.
-                Application.Current.Host.Settings.EnableFrameRateCounter = true;
+                Current.Host.Settings.EnableFrameRateCounter = true;
 
                 // Show the areas of the app that are being redrawn in each frame.
                 //Application.Current.Host.Settings.EnableRedrawRegions = true;
--- WindowsPhone7Examples/UiElements/MainPage.xaml.cs
+++ WindowsPhone7Examples/UiElements/MainPage.xaml.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using Microsoft.Phone.Controls;
 
 namespace UiElements
 {
-    public partial class MainPage : PhoneApplicationPage
+    public partial class MainPage
     {
-        // Constructor
         public MainPage()
         {
             InitializeComponent();
+
+            Loaded += MainPageLoaded;
+        }
+
+        void MainPageLoaded(object sender, RoutedEventArgs e)
+        {
+            var numericScope = new InputScope();
+            var numericScopeName = new InputScopeName();
+            numericScopeName.NameValue = InputScopeNameValue.Number;
+            numericScope.Names.Add(numericScopeName);
+            numberTextBox.InputScope = numericScope;           
         }
     }
 }
